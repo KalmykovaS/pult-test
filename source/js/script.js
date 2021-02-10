67e6ae7d51ef88dde71eb7d813386893515c4ece
Svetlana Kalmykova <79218609932@ya.ru>
2021-02-10 13:25:21 +0300

    add new project

---
+++ source/js/script.js
@@ -0,0 +1,46 @@
+$(document).ready(function(){
+  $('.gallery').slick({
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    infinite: false,
+    responsive: [
+      {
+        breakpoint: 1920,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 4,
+          infinite: false,
+        }
+      },
+      {
+        breakpoint: 1280,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          dots: true,
+          infinite: false
+        }
+      },
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          dots: true,
+          infinite: false
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          dots: true,
+          infinite: false,
+          variablewidth: false
+        }
+      }
+    ]
+  });
+});
+
